e53367979dac0f6be2403a8d436955487efd2883
lachlan-robinson <lachy4242@hotmail.com>
2025-03-17 18:16:13 +1100

    Fixed Id for polished stone production

--- higher-resource-production/mod.lua
+++ higher-resource-production/mod.lua
@@ -70,7 +70,7 @@ local buildingFunctions = {
     --PLANK
     { Id = "SAWMILL", Resources = { { Resource = "PLANK", Quantity = 1 } } },
     --POLISHED_STONE
-    { Id = "STONECUTTER_CAMP", Resources = { { Resource = "POLISHED_STONE", Quantity = 1 } } },
+    { Id = "STONEMASON_HUT", Resources = { { Resource = "POLISHED_STONE", Quantity = 1 } } },
     --QUARTZ
     { Id = "QUARRY_QUARTZ", Resources = { { Resource = "QUARTZ", Quantity = 1 } } },
     --SCULPTURE
